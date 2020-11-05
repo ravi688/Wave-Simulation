9fa72c5acffd30b81dadd8b9eb54cc959f8f840c
Ravi Prakash Singh <rp0412204@gmail.com>
2020-11-05 03:22:34 -0800

    Add files via upload

---
+++ Wave.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    private Transform[] bodies;
+    private MeshRenderer[] renderers;
+    private Vector3 disp;
+    public enum color
+    {
+        blue,
+        green,
+        red,
+        yellow,
+        white,
+    }
+
+    
+    public float amplitude = 2f;
+    public float angularfrequency = 5f;
+    public float constant = 5f;
+    public  float position = 0;
+    public float phase = 0;
+    public float jDistance = 0;
+    public float colorgradient;
+    public color __color;
+	// Use this for initialization
+	void Start () {
+        bodies = GetComponentsInChildren<Transform>();
+        renderers = GetComponentsInChildren<MeshRenderer>();
+        disp = new Vector3(0, 0, 0);
+        
+	}
+    
+    void Update()
+    {
+        foreach (Transform item in bodies)
+        {
+            
+            disp = item.position;
+           
+            disp.y = jDistance + wave(amplitude, angularfrequency, constant, item.position.x,phase);
+            item.position = disp;
+            
+        }
+        foreach (MeshRenderer item in renderers)
+        {
+            item.material.color = _color();
+        }
+    }
+    Color _color()
+    {
+        switch (__color)
+        {
+            case color.blue :
+                return Color.blue;
+            case color.white :
+                return Color.white;
+            case color.green :
+                return Color.green;
+            case color.red :
+                return Color.red; 
+            case color.yellow :
+                return Color.yellow;
+            default :
+                return Color.yellow;
+        }
+    }
+    float wave(float amp, float ang_freq, float constant,float pos, float phase = 0)
+    {
+        float disp = amp * Mathf.Sin(constant * pos - ang_freq * Time.time + phase);
+        return disp;
+    }
+}
---
+++ shm.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shm : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float meanpos;
+    private Vector3 MeanPos3;
+
+    public float mass;
+    public float k;
+    public float impulseMagnitude = 1f;
+
+    public float amplitude = 1f;
+    public float angularfrequency = 10f;
+    public float phase = Mathf.PI/10f;      
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.mass = mass;
+        meanpos = transform.position.y;
+        MeanPos3 = transform.position;
+	}
+    void Update()
+    {
+        MeanPos3.y += SHM_Eq(amplitude, angularfrequency, phase);
+        transform.position = MeanPos3;
+    }
+    float SHM_Eq(float amp, float ang_freq, float phase)
+    {
+        float disp = amp * Mathf.Sin(ang_freq * Time.time + phase);
+        return disp;
+    }
+    void SHM()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            impulse(impulseMagnitude);
+        force();
+    }
+    void force()
+    {
+
+        rb.AddForce(0, -k * (transform.position.y - meanpos), 0);
+    }
+    void impulse(float magnitude)
+    {
+        rb.AddForce(0, magnitude, 0);
+    }
+}
